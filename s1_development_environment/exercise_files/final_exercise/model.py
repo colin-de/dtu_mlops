259cc5dac8af85f2e25a67ec601352e3ba265199
Weihang Li <weihang.li@tum.de>
2024-01-04 14:17:27 +0100

    add s1

--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -7,3 +7,13 @@ class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(784, 128)
+
+myawesomemodel = nn.Sequential(
+    nn.Conv2d(1, 32, 3),  # [B, 1, 28, 28] -> [B, 32, 26, 26]
+    nn.LeakyReLU(),
+    nn.Conv2d(32, 64, 3), # [B, 32, 26, 26] -> [B, 64, 24, 24]
+    nn.LeakyReLU(),
+    nn.MaxPool2d(2),      # [B, 64, 24, 24] -> [B, 64, 12, 12]
+    nn.Flatten(),        # [B, 64, 12, 12] -> [B, 64 * 12 * 12]
+    nn.Linear(64 * 12 * 12, 10),
+)
